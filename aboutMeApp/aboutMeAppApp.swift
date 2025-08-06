e959077a60174394b11c5260266ed7d9d66a259d
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-06 13:07:20 -0700

    Initial Commit

---
+++ aboutMeApp/aboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeAppApp.swift
+//  aboutMeApp
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
