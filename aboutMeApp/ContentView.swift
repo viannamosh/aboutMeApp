023db42263114f809821ea2b87ef052d67746fce
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-06 13:51:58 -0700

    base project without any extras added

--- aboutMeApp/ContentView.swift
+++ aboutMeApp/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("background")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 600.0, height: 903.0)
+            VStack (spacing: 50){
+                ZStack{
+                RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 375.0, height: 230.0)
+                    .opacity(0.50)
+                    .colorInvert()
+                    HStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 200.0)
+                            .colorInvert()
+                        Image("sunset")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200.0, height: 200.0)
+                            .cornerRadius(50)
+                    }
+                    HStack{
+                        VStack{
+                            Text("Fun Fact: \n")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.pink)
+                            Text("I LOVE watching \n the sunset 🌅❤️‍🔥")
+                                .font(.headline)
+                                .multilineTextAlignment(.leading)
+                        }
+                       
+                        Text("                                            ")
+                    }
+                }
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 353.0, height: 186.0)
+                        .opacity(0.50)
+                        .colorInvert()
+                    Text("Vianna \n Moshtaghi")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                }
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 375.0, height: 230.0)
+                        .opacity(0.50)
+                        .colorInvert()
+                    
+                    HStack{
+                        Image("beach")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200.0, height: 200.0)
+                            .cornerRadius(50)
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 200.0)
+                            .colorInvert()
+                    }
+                    HStack{
+                        Text("                                            ")
+                        VStack{
+                            Text("Fun Fact: \n")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.pink)
+                            Text("I LOVE going to \n the beach 🌊💙")
+                                .font(.headline)
+                                .multilineTextAlignment(.trailing)
+                        }
+                    }
+                }
+            }
+    
+            .padding()
         }
-        .padding()
     }
 }
 
